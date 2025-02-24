c76b07e2863e8f9fbcb14f918dab63b61bfa378e
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-02-24 21:40:37 +0900

    update: emoji 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
 import { i18n } from './api/i18n'
 import { auth } from './auth'
+import { emoji } from './emoji'
 
 export const app = new Hono()
 
 app.route('/auth', auth)
 app.route('/api/i18n', i18n)
+app.route('/emoji', emoji)
 
 serve(app)
