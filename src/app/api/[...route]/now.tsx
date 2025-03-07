53789de25f18afa70d3edf1f676a65de02ffb878
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-03-07 10:14:23 +0900

    fix: `formatted` 추가

--- src/app/api/[...route]/now.tsx
+++ src/app/api/[...route]/now.tsx
@@ -1,10 +1,23 @@
 /** @jsxImportSource hono/jsx */
+import { format } from 'date-fns'
 import { Hono } from 'hono'
 
 export const now = new Hono()
 
 now.get('/', (c) => {
   const date = new Date()
+  const formatted = format(date, 'yyyy-MM-dd:HH:mm:ss')
 
-  return c.html(<pre>{JSON.stringify({ date }, null, 2)}</pre>)
+  return c.html(
+    <pre>
+      {JSON.stringify(
+        {
+          date,
+          formatted,
+        },
+        null,
+        2
+      )}
+    </pre>
+  )
 })
